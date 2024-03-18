201343a70bca4583511c556742eacc8ac7346fbc
softdev629 <mmeliukh71@gmail.com>
2024-03-18 16:06:35 -0400

    navbar responsive

--- js/main.js
+++ js/main.js
@@ -1,8 +1,33 @@
 /* =========== Show Navbar =========== */
+const navbar = document.querySelector(".navbar");
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", () => {
+  navbar.classList.toggle("show");
+});
 
 /* =========== Scroll Position =========== */
+const header = document.querySelector('.header');
+const scrollLink = document.querySelectorAll('.navbar a:not(:last-child)');
 
 /* =========== Smooth Scroll =========== */
+Array.from(scrollLink).map((link) => {
+  link.addEventListener('click', (e) => {
+    // Prevent Default
+    e.preventDefault();
+
+    const id = e.currentTarget.getAttribute('href').slice(1);
+    const element = document.getElementById(id);
+    let position = element.offsetTop - 90;
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+      behavior: 'smooth',
+    });
+    navbar.classList.remove('show');
+  });
+});
 
 /* =========== Preloader =========== */
 
